50a1e60450dad79dd4b63011e1eed650ad45f843
Nick Smith <96261639+nhsmith4@users.noreply.github.com>
2025-03-06 15:08:37 -0600

    Add files via upload

--- player_entry_screen.py
+++ player_entry_screen.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter as tk
+#import model.py as model
 
 root = tk.Tk()
 root.state("zoomed")
@@ -25,7 +26,7 @@ red_frame.pack(side="left", expand=True, fill="both")
 green_frame = tk.Frame(main_frame, bg="green")
 green_frame.pack(side="left", expand=True, fill="both")
 
-## Red side
+# Red side
 red_ids = tk.Frame(red_frame)
 red_ids.pack(side="left", expand=True, padx=10, pady=10)
 red_nicks = tk.Frame(red_frame)
@@ -40,7 +41,7 @@ for i in range(20):
     tk.Label(red_ids, text=f"ID {i+1}", bg="gray30", fg="white", width=20).pack(pady=1)
     tk.Label(red_nicks, text=f"Nickname {i+1}", bg="gray30", fg="white", width=20).pack(pady=1)
 
-## Green side
+# Green side
 green_ids = tk.Frame(green_frame)
 green_ids.pack(side="left", expand=True, padx=10, pady=10)
 green_nicks = tk.Frame(green_frame)
@@ -55,6 +56,7 @@ for i in range(20):
     tk.Label(green_ids, text=f"ID {i+1}", bg="gray30", fg="white", width=20).pack(pady=1)
     tk.Label(green_nicks, text=f"Nickname {i+1}", bg="gray30", fg="white", width=20).pack(pady=1)
 
+# Buttons at bottom of screen
 bottom_frame = tk.Frame(root, bg="black")
 bottom_frame.pack(fill="x")
 
@@ -64,6 +66,7 @@ game_mode_label.pack(side="left", padx=10, pady=20)
 fkeys_frame = tk.Frame(bottom_frame, bg="black")
 fkeys_frame.pack(side="right")
 
+### Button calls
 f1_label = tk.Label(fkeys_frame, text="F1\nEdit\nGame", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised")
 f2_label = tk.Label(fkeys_frame, text="F2\nGame\nParameters", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised")
 f3_label = tk.Label(fkeys_frame, text="F3\nStart\nGame", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised")
@@ -90,31 +93,35 @@ f10_label.pack(side="left", padx=5)
 f11_label.pack(side="left", padx=5)
 f12_label.pack(side="left", padx=5)
 
-root.mainloop()
-
-#buttons
-'''
+# Edit Game
 def on_f1_click(event):
     pass
 
+# Game Parameters
 def on_f2_click(event):
     pass
 
+# Start Game
 def on_f3_click(event):
     pass
 
+# PreEntered Games
 def on_f5_click(event):
     pass
 
+# N/A
 def on_f7_click(event):
     pass
 
+# View Game
 def on_f8_click(event):
     pass
 
+# Flick Sync
 def on_f10_click(event):
     pass
 
+# Clear Game
 def on_f12_click(event):
     pass
 
@@ -126,4 +133,5 @@ f7_label.bind(on_f7_click)
 f8_label.bind(on_f8_click)
 f10_label.bind(on_f10_click)
 f12_label.bind(on_f12_click)
-'''
+
+root.mainloop()
