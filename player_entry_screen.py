464252f3e8ec1967ba9c14047cd83f6a42699555
Nick Smith <96261639+nhsmith4@users.noreply.github.com>
2025-03-06 15:27:57 -0600

    Add files via upload

--- player_entry_screen.py
+++ player_entry_screen.py
@@ -66,72 +66,72 @@ game_mode_label.pack(side="left", padx=10, pady=20)
 fkeys_frame = tk.Frame(bottom_frame, bg="black")
 fkeys_frame.pack(side="right")
 
-### Button calls
-f1_label = tk.Label(fkeys_frame, text="F1\nEdit\nGame", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised")
-f2_label = tk.Label(fkeys_frame, text="F2\nGame\nParameters", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised")
-f3_label = tk.Label(fkeys_frame, text="F3\nStart\nGame", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised")
-f4_label = tk.Label(fkeys_frame, text="", fg="white", bg="black", height=4, width=9, borderwidth=5)
-f5_label = tk.Label(fkeys_frame, text="F5\nPreEntered\n Games", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised")
-f6_label = tk.Label(fkeys_frame, text="", fg="white", bg="black", height=4, width=9, borderwidth=5)
-f7_label = tk.Label(fkeys_frame, text="F7", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised")
-f8_label = tk.Label(fkeys_frame, text="F8\nView\nGame", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised")
-f9_label = tk.Label(fkeys_frame, text="", fg="white", bg="black", height=4, width=9, borderwidth=5)
-f10_label = tk.Label(fkeys_frame, text="F10\nFlick\nSync", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised")
-f11_label = tk.Label(fkeys_frame, text="", fg="white", bg="black", height=4, width=9, borderwidth=5)
-f12_label = tk.Label(fkeys_frame, text="F12\nClear\nGame", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised")
-
-f1_label.pack(side="left", padx=5)
-f2_label.pack(side="left", padx=5)
-f3_label.pack(side="left", padx=5)
-f4_label.pack(side="left", padx=5)
-f5_label.pack(side="left", padx=5)
-f6_label.pack(side="left", padx=5)
-f7_label.pack(side="left", padx=5)
-f8_label.pack(side="left", padx=5)
-f9_label.pack(side="left", padx=5)
-f10_label.pack(side="left", padx=5)
-f11_label.pack(side="left", padx=5)
-f12_label.pack(side="left", padx=5)
-
 # Edit Game
-def on_f1_click(event):
+def on_f1_click():
     pass
 
 # Game Parameters
-def on_f2_click(event):
+def on_f2_click():
     pass
 
 # Start Game
-def on_f3_click(event):
+def on_f3_click():
     pass
 
 # PreEntered Games
-def on_f5_click(event):
+def on_f5_click():
     pass
 
 # N/A
-def on_f7_click(event):
+def on_f7_click():
     pass
 
 # View Game
-def on_f8_click(event):
+def on_f8_click():
     pass
 
 # Flick Sync
-def on_f10_click(event):
+def on_f10_click():
     pass
 
 # Clear Game
-def on_f12_click(event):
+def on_f12_click():
     pass
 
-f1_label.bind(on_f1_click)
-f2_label.bind(on_f2_click)
-f3_label.bind(on_f3_click)
-f5_label.bind(on_f5_click)
-f7_label.bind(on_f7_click)
-f8_label.bind(on_f8_click)
-f10_label.bind(on_f10_click)
-f12_label.bind(on_f12_click)
+### Button calls
+f1_button = tk.Button(fkeys_frame, text="F1\nEdit\nGame", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised", command=on_f1_click)
+f2_button = tk.Button(fkeys_frame, text="F2\nGame\nParameters", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised", command=on_f2_click)
+f3_button = tk.Button(fkeys_frame, text="F3\nStart\nGame", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised", command=on_f3_click)
+f4_button = tk.Button(fkeys_frame, text="", fg="white", bg="black", height=4, width=9, borderwidth=5)
+f5_button = tk.Button(fkeys_frame, text="F5\nPreEntered\n Games", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised", command=on_f5_click)
+f6_button = tk.Button(fkeys_frame, text="", fg="white", bg="black", height=4, width=9, borderwidth=5)
+f7_button = tk.Button(fkeys_frame, text="F7", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised", command=on_f7_click)
+f8_button = tk.Button(fkeys_frame, text="F8\nView\nGame", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised", command=on_f8_click)
+f9_button = tk.Button(fkeys_frame, text="", fg="white", bg="black", height=4, width=9, borderwidth=5)
+f10_button = tk.Button(fkeys_frame, text="F10\nFlick\nSync", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised", command=on_f10_click)
+f11_button = tk.Button(fkeys_frame, text="", fg="white", bg="black", height=4, width=9, borderwidth=5)
+f12_button = tk.Button(fkeys_frame, text="F12\nClear\nGame", fg="white", bg="black", height=4, width=9, borderwidth=5, relief="raised", command=on_f12_click)
+
+f1_button.pack(side="left", padx=5)
+f2_button.pack(side="left", padx=5)
+f3_button.pack(side="left", padx=5)
+f4_button.pack(side="left", padx=5)
+f5_button.pack(side="left", padx=5)
+f6_button.pack(side="left", padx=5)
+f7_button.pack(side="left", padx=5)
+f8_button.pack(side="left", padx=5)
+f9_button.pack(side="left", padx=5)
+f10_button.pack(side="left", padx=5)
+f11_button.pack(side="left", padx=5)
+f12_button.pack(side="left", padx=5)
+
+'''f1_button.bind("<f1>", on_f1_click)
+f2_button.bind("<f2>", on_f2_click)
+f3_button.bind("<f3>", on_f3_click)
+f5_button.bind("<f5>", on_f5_click)
+f7_button.bind("<f7>", on_f7_click)
+f8_button.bind("<f8>", on_f8_click)
+f10_button.bind("<f10>", on_f10_click)
+f12_button.bind("<f12>", on_f12_click)'''
 
 root.mainloop()
