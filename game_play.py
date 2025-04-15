38f3fceb024bffb568df92eb1e2a8e13a526e72f
Nick Smith <nhsmith04@gmail.com>
2025-04-15 17:16:39 -0500

    Update game_play.py

--- game_play.py
+++ game_play.py
@@ -82,10 +82,7 @@ def create_frame(root):
         player_frame.pack(fill=tk.X, padx=10, pady=2)
         
         nickname = globe.model.red_nick[id].get()
-        if (globe.model.red_base_hit[id]) & (red_base_hit == False):
-            label_text = f"𝓑 {nickname}"
-            red_base_hit = True
-        else: label_text = nickname
+        
         player_label = tk.Label(player_frame, textvariable=globe.model.red_nick[id], font=("Arial", 14), bg="red", fg="white", anchor="w")
         player_label.pack(side=tk.LEFT, expand=True, fill=tk.X)
         
@@ -124,10 +121,7 @@ def create_frame(root):
         player_frame.pack(fill=tk.X, padx=10, pady=2)
         
         nickname = globe.model.green_nick[id].get()
-        if (globe.model.green_base_hit[id]) & (green_base_hit == False):
-            label_text = f"𝓑 {nickname}"
-            green_base_hit = True
-        else: label_text = nickname
+        
         player_label = tk.Label(player_frame, textvariable=globe.model.green_nick[id], font=("Arial", 14), bg="green", fg="white", anchor="w")
         player_label.pack(side=tk.LEFT, expand=True, fill=tk.X)
         
